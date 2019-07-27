4c2d3ebcc65cf56e43e31cdc89891cc02b26a3a4
Mikhail Gorbunov <toplenboren@yandex.ru>
2019-07-27 18:20:15 +0500

    Correct template procession

--- manpower-site/main/views.py
+++ manpower-site/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def main(request):
-    return HttpResponse('Hello, Misha. This is my first django experience!')
+    return render(request, 'manpower-site/home.html')
