8afb0b3fbbc33a3622952244522ef3cd6a22a2a4
Ilya_Bat9 <nasirov@olympus.ru>
2019-07-28 15:57:23 +0500

    Кривая работа формы

--- manpower-site/main/forms.py
+++ manpower-site/main/forms.py
@@ -5,3 +5,4 @@ from extractvacancy.models import Vacancy
 
 class CategoryForm(forms.Form):
     category_name = forms.ModelChoiceField(label=u'Минипрофессия', queryset=Vacancy.objects.values('name').distinct())
+    # заменить name на category_name
