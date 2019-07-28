32b0856cac1fa0650f7feb35ca9b4cae70792fc7
Mikhail Gorbunov <toplenboren@yandex.ru>
2019-07-28 13:20:39 +0500

    Add login page + little bit of forntend rework

--- manpower-site/main/urls.py
+++ manpower-site/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main)
+    path('', views.login, name="login"),
+    path('engine', views.main, name="engine")
 ]
